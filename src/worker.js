478fb3c9a86f21e06582ceaa86427a68143349cf
Christopher Giroir <kelsin@valefor.com>
2017-12-11 08:19:07 -0800

    Making update interval overridable

--- src/worker.js
+++ src/worker.js
@@ -1,14 +1,14 @@
 const geeklists = require('./geeklists');
 const logger = require('./logger');
 
-const INTERVAL = 1 * 60 * 1000;
+const INTERVAL = (process.env.UPDATE_INTERVAL_SECONDS || 5) * 1000;
 
 const update = () => {
   logger.info("Updating geeklists");
   return geeklists.updateGeeklists().reflect();
 }
 
-// Update initially and then start calling it every minute
+// Update initially and then start calling it based on the interval above
 update().then(() => setInterval(update, INTERVAL));
 
 logger.info("Worker thread started");
