761244b76f27cbaa376a5383146696184ecb1e57
Christopher Giroir <kelsin@valefor.com>
2017-12-11 00:38:18 -0800

    Fixing typo in worker

--- src/worker.js
+++ src/worker.js
@@ -5,7 +5,7 @@ const INTERVAL = 1 * 60 * 1000;
 
 const update = () => {
   logger.info("Updating geeklists");
-  return geeklists.updateGeeklists().relect();
+  return geeklists.updateGeeklists().reflect();
 }
 
 // Update initially and then start calling it every minute
